b5152670b46ea131e4a9bab7e4d3570e2c794548
Kawakaze <22422176jj@gmail.com>
2024-06-14 13:54:18 +0800

    update quiz Test

--- src/test/java/com/example/quiz/QuizServiceTests.java
+++ src/test/java/com/example/quiz/QuizServiceTests.java
@@ -140,5 +140,6 @@ public class QuizServiceTests {
             }
         }
         System.out.println(map);
+        System.out.println("=============================================");
     }
 }
